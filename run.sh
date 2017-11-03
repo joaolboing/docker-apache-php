7b6d55e3a1b5ed883b2f2e5def6208d1ce723932
João Boing <joaoboing@gmail.com>
2017-11-03 14:06:49 -0200

    REDIS_URL

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+RedisConfFile='/etc/php5/mods-available/redis.ini'
+if [[ ! -z "$REDIS_URL" ]] ; then
+  echo 'session.save_handler = redis' >> $RedisConfFile
+  echo 'session.save_path = ${REDIS_URL}' >> $RedisConfFile
+fi
+
+source /etc/apache2/envvars
+exec apache2 -D FOREGROUND
