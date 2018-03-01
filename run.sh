25d19d02520062ff9c4036ec82c897091be172e7
João Boing] <joao@conectra.net.br>
2018-03-01 11:56:49 -0300

    PHP_MEM_LIMIT

--- run.sh
+++ run.sh
@@ -7,5 +7,10 @@ if [[ ! -z "$REDIS_URL" ]] ; then
   echo 'session.save_path = ${REDIS_URL}' >> $RedisConfFile
 fi
 
+PhpIniFile='/etc/php5/apache2/php.ini'
+if [[ ! -z "$PHP_MEM_LIMIT" ]] ; then
+	sed -i 's/^memory_limit = .*/memory_limit = '$PHP_MEM_LIMIT'/' $PhpIniFile
+fi
+
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
